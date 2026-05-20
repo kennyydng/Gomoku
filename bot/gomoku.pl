74d6355bdde2e6ff8f2e6b73066e75d24987505a
Neyl JABER <njaber@f2r10s22.clusters.42paris.fr>
2026-05-20 17:41:55 +0200

    Added prolog code and docker compose file

---
+++ bot/gomoku.pl
@@ -0,0 +1,42 @@
+
+:- module(gomoku, [
+	play/3
+]).
+
+:- prolog_flag(optimise,true).
+
+cell(-). cell(1). cell(2).
+
+:- multifile error:has_type/2.
+error:has_type(board,Board) :-
+	functor(Board,'.',19),
+	forall(arg(_,Board,Row), (
+		functor(Row,'.',19),
+		forall(arg(_,Row,Cell), cell(Cell))
+	)).
+
+
+capture(_,_,_) :- false.
+
+:- meta_predicate with_move(+,0).
+
+:- det(play/3).
+
+play(State,X,Y) :-
+	State = gomoku(Turn,Points,Board),
+
+	Player is (Turn mod 2) + 1,
+	$ arg(Y,Board,Row), $ arg(X,Row,'-'),
+	setarg(X,Row,Player),
+
+	succ(Turn,Turn1),
+	setarg(1,State,Turn1),
+
+	aggregate_all(count, capture(Board,X,Y), Captures),
+	arg(Player,Points,OldPoints),
+	NewPoints is OldPoints + Captures,
+	setarg(Player,Points,NewPoints).
+
+user:portray(gomoku(Turn,Points,_)) :-
+	nonvar(Turn), nonvar(Points), !, Points = P0-P1,
+	format("<Turn ~d | ~d-~d>",[Turn,P0,P1]).
